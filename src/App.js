b5b04732af9a47ccc3ba3766161702c5dfa50d66
marciobarrosetec <marcio.barros.etec@gmail.com>
2025-11-24 14:56:34 -0300

    Atualização teste

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   const value = 'Marcio Alberto';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}
+          <p>Essa é uma simples pagina com react e azure.</p>
+  
+        </div>;
 }
 
 export default App;
