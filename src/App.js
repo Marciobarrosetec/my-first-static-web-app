9abe8af61d29126a4f38085ba1e8a81878b05552
marciobarrosetec <marcio.barros.etec@gmail.com>
2025-12-01 13:35:13 -0300

    New teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'Marcio Alberto';
+  const value = 'Marcio Alberto de barros';
   return <div>Hello {value}
           <p>Essa é uma simples pagina com react e azure.</p>
   
